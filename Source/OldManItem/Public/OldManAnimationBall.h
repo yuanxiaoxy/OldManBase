62a3d3813d4675ced93c8ec72344783e71a6570a
helimu <feirenzai_li@163.com>
2025-11-30 01:31:13 +0800

    动画球功能实现学习

---
+++ Source/OldManItem/Public/OldManAnimationBall.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase/OldManInterectItemBase.h"
+#include "OldManAnimationBall.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OLDMANITEM_API AOldManAnimationBall : public AOldManInterectItemBase
+{
+	GENERATED_BODY()
+	
+};

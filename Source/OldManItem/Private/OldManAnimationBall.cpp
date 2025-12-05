8477d41f4e54ed924db8a37be9be95a35a2403b3
helimu <feirenzai_li@163.com>
2025-12-05 19:33:01 +0800

    动画球制作

--- Source/OldManItem/Private/OldManAnimationBall.cpp
+++ Source/OldManItem/Private/OldManAnimationBall.cpp
@@ -1,5 +1,40 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "OldManAnimationBall.h"
 
+void AOldManAnimationBall::PlayAniInScene()
+{
+	UE_LOG(LogTemp, Display, TEXT("scene"));
+}
+
+void AOldManAnimationBall::PlayAniInUI()
+{
+	UE_LOG(LogTemp, Display, TEXT("UI"));
+}
+
+void AOldManAnimationBall::PlayText()
+{
+	UE_LOG(LogTemp, Display, TEXT("text"));
+}
+
+void AOldManAnimationBall::OnOverlayBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlayBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	switch (myType)
+	{
+		case E_AniBallType::playOnScene:
+			PlayAniInScene();
+			break;
+		case E_AniBallType::playOnUI:
+			PlayAniInUI();
+			break;
+		case E_AniBallType::playAsText:
+			PlayText();
+			break;
+		default:
+			UE_LOG(LogTemp, Warning, TEXT("你不用，还不删，留着过年呢"));
+			break;
+	}
+}
--- Source/OldManItem/Public/OldManAnimationBall.h
+++ Source/OldManItem/Public/OldManAnimationBall.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,6 +6,14 @@
 #include "ItemBase/OldManInterectItemBase.h"
 #include "OldManAnimationBall.generated.h"
 
+UENUM(BlueprintType)
+enum class E_AniBallType : uint8
+{
+	None UMETA(DisplayName = "None"),
+	playOnScene UMETA(DisplayName = "Play On Scene"),
+	playOnUI UMETA(DisplayName = "Play On UI"),
+	playAsText UMETA(DisplayName = "Play As Text")
+};
 /**
  * 
  */
@@ -13,5 +21,29 @@ UCLASS()
 class OLDMANITEM_API AOldManAnimationBall : public AOldManInterectItemBase
 {
 	GENERATED_BODY()
-	
+public:
+	//动画球播放类型
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimationBallType")
+	E_AniBallType myType = E_AniBallType::playOnScene;
+	//是否循环
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimationBallType")
+	bool Loop = false;
+	//是否取消玩家输入
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimationBallType")
+	bool PlayerInputCancel = true;
+	//对话框是否自动播放
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimationBallType",
+		meta = (EditCondition = "myType == AniBallType::playAsText"))
+	bool IsAutoText = true;
+
+
+
+private:
+	void PlayAniInScene();
+	void PlayAniInUI();
+	void PlayText();
+
+protected:
+	virtual void OnOverlayBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
 };

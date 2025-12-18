c943fb11ce2a6a44657d8e3eef20846c0da8982b
helimu <feirenzai_li@163.com>
2025-12-18 22:17:39 +0800

    UI串联

--- Source/OldManUI/Public/SettingPanel/SettingPanel.h
+++ Source/OldManUI/Public/SettingPanel/SettingPanel.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -9,9 +9,24 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable)
 class OLDMANUI_API USettingPanel : public UUIBase
 {
 	GENERATED_BODY()
 	
+
+protected:
+	UPROPERTY(BlueprintReadWrite, Category = "MyCategory")
+	float BGMVolume = 1;
+	UPROPERTY(BlueprintReadWrite, Category = "MyCategory")
+	float EffectVolume = 1;
+
+	UFUNCTION(BlueprintCallable, Category = "MyCategory")
+	void UpdateBGMToUI(float BGMVol);
+	UFUNCTION(BlueprintCallable, Category = "MyCategory")
+	void UpdateEffectToUI(float EffectVol);
+	UFUNCTION(BlueprintCallable, Category = "MyCategory")
+	void UpdateBGMToGame();
+	UFUNCTION(BlueprintCallable, Category = "MyCategory")
+	void UpdateEffectToGame();
 };
